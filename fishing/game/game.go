c0d867dc3d54280f1e195bc0003a816ea3c95194
Sidd Viswanathan <siddharth.viswanathan@gmail.com>
2026-01-27 00:36:21 -0500

    Beginning of Store

--- fishing/game/game.go
+++ fishing/game/game.go
@@ -6,6 +6,7 @@ import (
 	"image/color"
 	"log"
 	"os"
+	"strconv"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/examples/resources/fonts"
@@ -29,6 +30,13 @@ type Game struct {
 	save       *Save
 }
 
+type Shop struct {
+	ShopMode     ShopMode
+	selectedFish string
+	selectedItem string
+	quantity     int
+}
+
 type Mode int
 
 const (
@@ -36,6 +44,13 @@ const (
 	Animation
 	Shopping
 	Fishing
+)
+
+type ShopMode bool
+
+const (
+	Buying  ShopMode = true
+	Selling ShopMode = false
 )
 
 func init() {
@@ -51,7 +66,19 @@ func init() {
 		log.Fatal(err)
 	}
 
-	startButton = gamecommon.NewRectangleButton("start", ScreenWidth/2-100, 5*titleFontSize, 200, 50, "Click to Start", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	fishingButton = gamecommon.NewRectangleButton("fish", ScreenWidth/2-100, 5*titleFontSize, 200, 50, "Click to Start", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	shoppingButton = gamecommon.NewRectangleButton("shop", ScreenWidth/2-100, 7*titleFontSize, 200, 50, "Click to Shop", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	quitButton = gamecommon.NewRectangleButton("quit", ScreenWidth/2-100, 9*titleFontSize, 200, 50, "Click to Quit", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	for fish := range fishes {
+		fishButton := gamecommon.NewRectangleButton(fish, 0, 0, 200, 50, fish, arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+		fishButtons = append(fishButtons, fishButton)
+	}
+	buyButton = gamecommon.NewRectangleButton("buy", ScreenWidth/2-200, 1*titleFontSize, 200, 50, "Buy", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	sellButton = gamecommon.NewRectangleButton("sell", ScreenWidth/2, 1*titleFontSize, 200, 50, "Sell", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	leaveButton = gamecommon.NewRectangleButton("leave", ScreenWidth/2-200, 16*titleFontSize, 100, 50, "Leave", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	plusButton = gamecommon.NewRectangleButton("plus", ScreenWidth/2-80, 14*titleFontSize, 60, 50, "+", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	minusButton = gamecommon.NewRectangleButton("minus", ScreenWidth/2-200, 14*titleFontSize, 60, 50, "-", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
+	confirmButton = gamecommon.NewRectangleButton("confirm", ScreenWidth/2, 14*titleFontSize, 200, 50, "Confirm", arcadeFaceSource, color.White, color.RGBA{0, 0, 255, 255}, input)
 }
 
 var (
@@ -59,7 +86,23 @@ var (
 	fishingAnimationFrames []*ebiten.Image
 	framecounter           int
 	input                  *gamecommon.Input
-	startButton            gamecommon.Button
+
+	//title buttons
+	fishingButton  gamecommon.Button
+	shoppingButton gamecommon.Button
+	quitButton     gamecommon.Button
+
+	//shopping buttons
+	buyButton     gamecommon.Button
+	sellButton    gamecommon.Button
+	fishButtons   []gamecommon.Button
+	storeButtons  []gamecommon.Button
+	leaveButton   gamecommon.Button
+	plusButton    gamecommon.Button
+	minusButton   gamecommon.Button
+	confirmButton gamecommon.Button
+
+	//fishing buttons
 )
 
 type Save struct {
@@ -69,6 +112,7 @@ type Save struct {
 }
 
 var save *Save
+var shop *Shop
 
 // NewGame generates a new Game object.
 func NewGame() (*Game, error) {
@@ -82,6 +126,12 @@ func NewGame() (*Game, error) {
 			}
 		}
 	}
+	shop = &Shop{
+		selectedFish: "",
+		selectedItem: "",
+		quantity:     0,
+	}
+
 	g := &Game{
 		input: input,
 		save:  save,
@@ -94,19 +144,81 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeigh
 	return ScreenWidth, ScreenHeight
 }
 
+func (shop *Shop) increaseQuantity() {
+	shop.quantity++
+}
+
+func (shop *Shop) dereaseQuantity() {
+	if shop.quantity > 0 {
+		shop.quantity--
+	}
+}
+
+func (shop *Shop) resetSelection() {
+	shop.selectedFish = ""
+	shop.selectedItem = ""
+	shop.quantity = 0
+}
+
+func (shop *Shop) makePurchase() {
+	if shop.ShopMode == Buying {
+		price := fishes[shop.selectedFish].Price
+		totalCost := price * shop.quantity
+		save.money -= totalCost
+	}
+}
+
 // Update updates the current game state.
 func (g *Game) Update() error {
+	g.input.Update()
 	switch g.mode {
 	case Title:
-		g.input.Update()
-		pressed := startButton.IsPressed()
+		pressed := fishingButton.IsPressed()
 		if pressed {
 			g.mode = Animation
+		}
+		pressed = shoppingButton.IsPressed()
+		if pressed {
+			shop.ShopMode = Buying
+			g.mode = Shopping
+		}
+		pressed = quitButton.IsPressed()
+		if pressed {
+			return ebiten.Termination
 		}
 		return nil
 	case Animation:
 		return nil
 	case Shopping:
+		pressed := buyButton.IsPressed()
+		if pressed {
+			shop.ShopMode = Buying
+			shop.resetSelection()
+		}
+		pressed = sellButton.IsPressed()
+		if pressed {
+			shop.ShopMode = Selling
+			shop.resetSelection()
+		}
+		pressed = plusButton.IsPressed()
+		if pressed {
+			g.mode = Shopping
+			shop.increaseQuantity()
+		}
+		pressed = minusButton.IsPressed()
+		if pressed {
+			g.mode = Shopping
+			shop.dereaseQuantity()
+		}
+		pressed = leaveButton.IsPressed()
+		if pressed {
+			g.mode = Title
+			shop.resetSelection()
+		}
+		pressed = confirmButton.IsPressed()
+		if pressed {
+			g.mode = Shopping
+		}
 		return nil
 	case Fishing:
 		return nil
@@ -115,6 +227,10 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) drawTitle(screen *ebiten.Image) {
+	fishingButton.Draw(screen)
+	shoppingButton.Draw(screen)
+	quitButton.Draw(screen)
+
 	titleTexts := "FISHING GAME"
 	op := &text.DrawOptions{}
 	op.GeoM.Translate(ScreenWidth/2, 3*titleFontSize)
@@ -125,8 +241,6 @@ func (g *Game) drawTitle(screen *ebiten.Image) {
 		Source: arcadeFaceSource,
 		Size:   titleFontSize,
 	}, op)
-
-	startButton.Draw(screen)
 
 	const msg = "Fishing Game by Sidd Viswanathan"
 
@@ -156,7 +270,22 @@ func (g *Game) drawAnimation(screen *ebiten.Image) {
 }
 
 func (g *Game) drawShopping(screen *ebiten.Image) {
+	buyButton.Draw(screen)
+	sellButton.Draw(screen)
+	leaveButton.Draw(screen)
+	plusButton.Draw(screen)
+	minusButton.Draw(screen)
+	confirmButton.Draw(screen)
 
+	op := &text.DrawOptions{}
+	op.GeoM.Translate(ScreenWidth/2-110, 14*titleFontSize+10)
+	op.ColorScale.ScaleWithColor(color.White)
+	op.LineSpacing = titleFontSize
+	op.PrimaryAlign = text.AlignCenter
+	text.Draw(screen, strconv.Itoa(shop.quantity), &text.GoTextFace{
+		Source: arcadeFaceSource,
+		Size:   24,
+	}, op)
 }
 
 func (g *Game) drawFishing(screen *ebiten.Image) {
